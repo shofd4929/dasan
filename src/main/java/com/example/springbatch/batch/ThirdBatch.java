3bcb5160139f25bb9a2d93411572d2c224cdcdc4
kimjihun <xxxjjhhh@naver.com>
2024-08-01 00:32:42 +0900

    * 배치 3 완료

---
+++ src/main/java/com/example/springbatch/batch/ThirdBatch.java
@@ -0,0 +1,10 @@
+package com.example.springbatch.batch;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThirdBatch {
+
+    //https://github.com/spring-projects/spring-batch/tree/main/spring-batch-samples/src/main/java/org/springframework/batch/samples
+}
+
