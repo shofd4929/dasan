15f456fa079b3ae7ad0c5b6b66f69001ad5ea27b
Dasan <shofd@dasansystem.com>
2024-12-11 17:22:57 +0900

    master

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class SpringBatchApplication {
 
+    // 마스터 브랜치
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchApplication.class, args);
     }
