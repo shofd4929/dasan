08ad976617f58101620702fe9cbc9634f354eda0
shin <id7886@naver.com>
2024-12-18 16:22:52 +0900

    1

--- src/main/java/com/example/springbatch/repository/OtpRepository.java
+++ src/main/java/com/example/springbatch/repository/OtpRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 
 public interface OtpRepository extends JpaRepository<OTPINFO, Long> {
-    void deleteByOtpdateBefore(LocalDate date);
 }
