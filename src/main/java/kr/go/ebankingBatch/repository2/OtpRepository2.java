9f0524bfa51d3d9ce27946b99252055b35a96576
Dasan <shofd@dasansystem.com>
2024-12-18 13:36:35 +0900

    Oracle_1DB_TESTSERVER

--- src/main/java/kr/go/ebankingBatch/repository2/OtpRepository2.java
+++
@@ -1,8 +0,0 @@
-package kr.go.ebankingBatch.repository2;
-
-import kr.go.ebankingBatch.entity2.OTPINFO2;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OtpRepository2 extends JpaRepository<OTPINFO2, Long> {
-
-}
