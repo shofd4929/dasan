6a64a35333b3ff85240d4a621fc4ecd4fd50219c
shin <id7886@naver.com>
2024-12-19 10:22:49 +0900

    1

--- src/main/java/kr/go/ebankingBatch/entity/OTPINFO.java
+++ src/main/java/kr/go/ebankingBatch/entity/OTPINFO.java
@@ -1,8 +1,6 @@
 package kr.go.ebankingBatch.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.Date;
@@ -16,5 +14,6 @@ public class OTPINFO {
     @Column(name = "otpcode")
     private int id;
 
+    @Temporal(TemporalType.DATE)
     private Date otpdate;
 }
