df31b5ce174ef1f1633ba92ba2f297cb61a031ee
shin <id7886@naver.com>
2024-12-20 17:50:48 +0900

    1

---
+++ src/main/java/kr/go/ebankingBatch/config/JasyptConfig.java
@@ -0,0 +1,26 @@
+package kr.go.ebankingBatch.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(){
+        String key = "KScjy3pKDd8v0LYXs2qGqS9H0NrF0iL/9TBol4RKFeY1ciek43GIbReHC2uvshSd";
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(key);
+
+        config.setAlgorithm("PBEWITHMD5ANDDES");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
---
+++ src/test/java/kr/go/ebankingBatch/JasyptTest.java
@@ -0,0 +1,24 @@
+package kr.go.ebankingBatch;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+
+public class JasyptTest {
+
+    @Test
+    public void jasyptTest(){
+        String value = "1111";
+        String result = jasyptEncoding(value);
+        System.out.println(result);
+    }
+
+    public String jasyptEncoding(String value){
+        String key = "KScjy3pKDd8v0LYXs2qGqS9H0NrF0iL";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWITHMD5ANDDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
+}
