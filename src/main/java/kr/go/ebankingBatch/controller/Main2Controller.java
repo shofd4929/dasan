680969abd7fda7c741ca92fb1a81724e95219ddc
shin <id7886@naver.com>
2025-01-02 15:44:15 +0900

    1

--- src/main/java/kr/go/ebankingBatch/controller/Main2Controller.java
+++ src/main/java/kr/go/ebankingBatch/controller/Main2Controller.java
@@ -20,25 +20,25 @@ public class Main2Controller {
 
     private final JobLauncher jobLauncher;
     private final JobRegistry jobRegistry;
-    private final ThreadPoolTaskScheduler taskScheduler; // 스케줄러 관리
     private ScheduledFuture<?> scheduledFuture; // 스케줄된 작업을 관리할 변수
 
     public Main2Controller(JobLauncher jobLauncher, JobRegistry jobRegistry, ThreadPoolTaskScheduler taskScheduler) {
         this.jobLauncher = jobLauncher;
         this.jobRegistry = jobRegistry;
-        this.taskScheduler = taskScheduler;
     }
 
     // 배치 작업 시작 API
     @GetMapping("/startOtpJob")
     public String startOtpJob(@RequestParam("value") String value) throws Exception {
         // 배치 작업 실행
-        /*JobParameters jobParameters = new JobParametersBuilder()
-                .addString("date", value)
-                .toJobParameters();
-
-        // 배치 작업 실행
-        jobLauncher.run(jobRegistry.getJob("otpJob"), jobParameters);*/
+        /*
+         * JobParameters jobParameters = new JobParametersBuilder()
+         * .addString("date", value)
+         * .toJobParameters();
+         * 
+         * // 배치 작업 실행
+         * jobLauncher.run(jobRegistry.getJob("otpJob"), jobParameters);
+         */
 
         return "OTP 배치 작업이 시작되었습니다.";
     }
@@ -47,7 +47,7 @@ public class Main2Controller {
     @GetMapping("/stopOtpJob")
     public String stopOtpJob() {
         if (scheduledFuture != null && !scheduledFuture.isCancelled()) {
-            scheduledFuture.cancel(false);  // 실행 중인 작업을 취소합니다.
+            scheduledFuture.cancel(false); // 실행 중인 작업을 취소합니다.
             return "OTP 배치 작업이 중지되었습니다.";
         }
         return "OTP 배치 작업이 이미 중지되었습니다.";
@@ -56,8 +56,6 @@ public class Main2Controller {
     // 주기적으로 실행되는 OTP 배치 작업 (기존 스케줄 작업)
     @GetMapping("/scheduleOtpJob")
     public String scheduleOtpJob() {
-        // 스케줄 작업 실행
-        scheduledFuture = taskScheduler.schedule(this::runOtpJob, new Date(System.currentTimeMillis())); // 새로 스케줄을 시작
         return "OTP 배치 작업이 새로 시작되었습니다.";
     }
 
--- src/main/java/kr/go/ebankingBatch/controller/MainController.java
+++ src/main/java/kr/go/ebankingBatch/controller/MainController.java
@@ -1,11 +1,9 @@
 package kr.go.ebankingBatch.controller;
 
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
